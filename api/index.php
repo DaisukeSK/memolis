1d4f9e11a7829fd9c81b69ee1c19107ab7a95070
Daisuke <skd.itkn@gmail.com>
2024-05-03 15:17:24 -0700

    c

---
+++ api/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link href="/css/index.css" rel="stylesheet">
+    
+</head>
+
+<body>
+    <h2>php TEST 2</h2>
+
+    <form action="/test" method="post">
+        <input type="text" name="username" required>
+        <input type="submit">
+    </form>
+
+    <form action="/test2" method="post">
+        <input type="text" name="username" required>
+        <input type="submit">
+    </form>
+
+    <!-- <form action="/test" method="post">
+        <input type="text" name="username" required>
+        <input type="submit">
+    </form>
+
+    <form action="/test2" method="post">
+        <input type="text" name="username" required>
+        <input type="submit">
+    </form> -->
+
+</body>
+</html>
