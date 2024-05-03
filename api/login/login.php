86b7d994b510d9f972f4aa2e107b513afffef7a9
Daisuke <skd.itkn@gmail.com>
2024-05-03 14:42:03 -0700

    cmt

--- api/login/login.php
+++ api/login/login.php
@@ -11,7 +11,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link href="/css/common.css" rel="stylesheet">
+    <link href="../../css/common.css" rel="stylesheet">
     <link href="../../css/login.css" rel="stylesheet">
     <link href="../assets/svg/memolis.ico" rel="shortcut icon">
     <link href="../assets/svg/memolis.ico" rel="icon">
